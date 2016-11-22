363261ff6b56c01d408f86b117d371647d3731c7
Jacob Meline <jacob.meline@gmail.com>
2016-11-22 08:32:23 -0700

    Adding starting project

---
+++ src/Sample.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Sample.Test
+{
+    public class Program
+    {
+        [Fact]
+        public void Test1()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                i.ShouldBe(i);
+            }
+        }
+    }
+}
